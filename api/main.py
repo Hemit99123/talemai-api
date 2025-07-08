3447102e09a4340382221863a0d6cea7977971c6
Hemit Patel <hemitvpatel@gmail.com>
2025-07-08 09:33:09 -0400

    update handling of chat stuff

--- api/main.py
+++ api/main.py
@@ -2,14 +2,25 @@
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from helper import ai
+from langchain_astradb import AstraDBVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
 from pydantic import BaseModel
 from helper import auth
 from helper import cookie 
 from decorators import precheck
+from helper.ai import query_model
+from dotenv import load_dotenv
+from os import getenv
 
 app = FastAPI()
 
+
+load_dotenv()
+
+ASTRA_DB_API_ENDPOINT = getenv("ASTRA_DB_API_ENDPOINT")
+ASTRA_DB_APPLICATION_TOKEN = getenv("ASTRA_DB_APPLICATION_TOKEN")
+ASTRA_DB_NAMESPACE = getenv("ASTRA_DB_NAMESPACE")
+GROQ_API_KEY = getenv("GROQ_API_KEY")
 
 class QueryModal(BaseModel):
     """Schema for incoming POST request with user query."""
@@ -34,24 +45,32 @@ async def handle_index_request(request : Request):
     """Health check route for the server."""
     return {"response": "Talem AI server"}
 
-
-import asyncio
-
 @app.post("/chat/")
 @precheck.decorator
 async def handle_chat_request(request: Request):
     """Handle POST request, process query, and return AI-generated response."""
-    import asyncio
+
     data = await request.json()
-    user_query = data.get("query")
-    try:
-        # Set a timeout to prevent hanging
-        ai_response = await asyncio.wait_for(ai.fetch_and_query(user_query), timeout=30)
-        return {"response": ai_response}
-    except asyncio.TimeoutError:
-        return {"response": "The request timed out. Please try again later."}
-    except Exception as e:
-        return {"response": f"An error occurred: {str(e)}"}
+    query = data.get("query")
+
+    embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(model_name=embedding_model)
+
+    vectorstore = AstraDBVectorStore(
+        collection_name="main_v2",
+        embedding=embeddings,
+        api_endpoint=ASTRA_DB_API_ENDPOINT,
+        token=ASTRA_DB_APPLICATION_TOKEN,
+        namespace=ASTRA_DB_NAMESPACE,
+    )
+
+    print(f"Querying vectorstore for: {query}")
+
+    retriever = vectorstore.as_retriever()
+    retrieved_docs = retriever.invoke(query)
+    context = "\n\n".join([doc.page_content for doc in retrieved_docs])
+
+    return { "response": await query_model(context, query) }
 
 
 @app.post("/login/")
