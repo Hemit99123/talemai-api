7cff2a351b6ff1704bdeb36a7c5b8452a3bf6be2
Hemit Patel <hemitvpatel@gmail.com>
2025-07-09 12:35:11 -0400

    fix linting errors

--- api/helper/mongodb.py
+++ api/helper/mongodb.py
@@ -1,8 +1,11 @@
-from pymongo import MongoClient
-from os import getenv 
+"""Helper module to initialize and return a MongoDB client."""
+
+from os import getenv  # Standard imports first
+from pymongo import MongoClient  # Third-party imports next
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 
 def get_mongo_client():
+    """Return a MongoDB client using the configured URI."""
     client = MongoClient(MONGO_DB_URI)
     return client
